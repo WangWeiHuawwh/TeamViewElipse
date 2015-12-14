313735c6c6a6eb1b6adb091ebf7443d1f55e9c7a
wangweihua <624214399@qq.com>
2015-12-14 17:51:42 +0800

    .

--- gen/com/example/teamview/R.java
+++ gen/com/example/teamview/R.java
@@ -23,6 +23,7 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int action_settings=0x7f090000;
@@ -36,6 +37,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f060002;
         public static final int app_name=0x7f060000;
+        public static final int description=0x7f060003;
         public static final int hello_world=0x7f060001;
     }
     public static final class style {
